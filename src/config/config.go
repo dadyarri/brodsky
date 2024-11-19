2f0212bcd6ae724ccfc92936b02c7545437967b1
Голубев Д.О. <golubev@sicon.ru>
2024-11-19 14:57:35 +0300

    add some fields to configuration

--- src/config/config.go
+++ src/config/config.go
@@ -7,9 +7,15 @@ type Config struct {
 	Author      string                 `toml:"author" comment:"Author of the site"`
 	Taxonomies  []Taxonomy             `toml:"taxonomies" comment:"Taxonomies of the site (i. e. tags, categories, etc)"`
 	OutputPath  string                 `toml:"output_path" comment:"Path to the output directory (defaults to public)"`
+	CompileSass bool                   `toml:"compile_sass" comment:"Compile Sass files from sass folder to CSS"`
+	Markdown    Markdown               `toml:"markdown"`
 	Extra       map[string]interface{} `toml:"extra" comment:"Extra information about the site"`
 }
 
 type Taxonomy struct {
 	Name string
+}
+
+type Markdown struct {
+	SyntaxHighlighting bool `toml:"highlight_code" comment:"Whether to do syntax highlighting"`
 }
