33b16e4767b293fcc4e6a6a8f9b8bf6e74ebcb4f
Голубев Д.О. <golubev@sicon.ru>
2024-11-19 14:57:46 +0300

    add some stuff to init command

--- src/internal/handlers/init.go
+++ src/internal/handlers/init.go
@@ -1,15 +1,20 @@
 package handlers
 
 import (
-	config "brodsky/config"
+	"brodsky/config"
 	"brodsky/internal"
+	"brodsky/internal/info"
 	"brodsky/internal/log"
+	"errors"
 	"fmt"
+	"github.com/manifoldco/promptui"
 	"github.com/pelletier/go-toml/v2"
 	"github.com/spf13/cobra"
+	"net/url"
 	"os"
 	"os/user"
 	"path/filepath"
+	"strings"
 )
 
 func InitRunE(cmd *cobra.Command, _ []string) error {
@@ -21,11 +26,57 @@ func handleInitRun(cmd *cobra.Command) error {
 	projectName := cmd.Flag("name").Value.String()
 	force, err := cmd.Flags().GetBool("force")
 
-	log.Info(fmt.Sprintf("Initializing new project '%s'", projectName))
-
 	if err != nil {
 		return err
 	}
+
+	log.Info(fmt.Sprintf("Initializing new project '%s'", projectName))
+	log.Info(fmt.Sprintf("Welcome to %s!", info.GetAppName()))
+	log.Info("Please answer a few questions to get started quickly.")
+
+	urlPrompt := promptui.Prompt{
+		Label: "What is the URL of your site?",
+		Validate: func(input string) error {
+			u, err := url.Parse(input)
+			if err == nil && u.Scheme != "" && u.Host != "" {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	urlResult, err := urlPrompt.Run()
+
+	sassPrompt := promptui.Prompt{
+		Label: "Do you want to enable Sass compilation? [Y/n]",
+		Validate: func(input string) error {
+			lower := strings.ToLower(input)
+
+			if lower == "y" || lower == "n" {
+				return nil
+			}
+
+			return errors.New("y/n is only valid answers")
+		},
+	}
+
+	sassResult, err := sassPrompt.Run()
+
+	syntaxPrompt := promptui.Prompt{
+		Label: "Do you want to enable syntax highlighting? [Y/n]",
+		Validate: func(input string) error {
+			lower := strings.ToLower(input)
+
+			if lower == "y" || lower == "n" || lower == "" {
+				return nil
+			}
+
+			return errors.New("y/n is only valid answers")
+		},
+	}
+
+	syntaxResult, err := syntaxPrompt.Run()
 
 	projectBasePath, err := os.Getwd()
 
@@ -56,6 +107,24 @@ func handleInitRun(cmd *cobra.Command) error {
 		return err
 	}
 
+	compileSass := sassResult == "y"
+
+	if compileSass {
+		err = os.MkdirAll(filepath.Join(projectPath, "sass"), os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	directories := []string{"content", "themes", "templates", "static"}
+
+	for _, dir := range directories {
+		err = os.MkdirAll(filepath.Join(projectPath, dir), os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
 	usr, err := user.Current()
 
 	if err != nil {
@@ -63,10 +132,15 @@ func handleInitRun(cmd *cobra.Command) error {
 	}
 
 	cfg := config.Config{
-		Title:      projectName,
-		Author:     usr.Username,
-		Taxonomies: make([]config.Taxonomy, 0),
-		OutputPath: "public",
+		BaseUrl:     urlResult,
+		Title:       projectName,
+		Author:      usr.Username,
+		Taxonomies:  make([]config.Taxonomy, 0),
+		OutputPath:  "public",
+		CompileSass: compileSass,
+		Markdown: config.Markdown{
+			SyntaxHighlighting: syntaxResult == "y",
+		},
 	}
 
 	b, err := toml.Marshal(cfg)
