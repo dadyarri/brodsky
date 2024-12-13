aab4eebdaa9e51057a8ac1bcfd412935cf3efc9a
Daniil <dadyarri@gmail.com>
2024-12-13 08:48:49 +0300

    add util to construct relative paths to some base

--- src/pkg/utils/file.go
+++ src/pkg/utils/file.go
@@ -1,6 +1,10 @@
 package utils
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func PathExists(path string) (bool, error) {
 	_, err := os.Stat(path)
@@ -11,4 +15,24 @@ func PathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+// RelativizePath checks if the targetPath is absolute.
+// If it is, it constructs the relative path from basePath to targetPath.
+func RelativizePath(basePath, targetPath string) (string, error) {
+	// Ensure both basePath and targetPath are absolute paths
+	if !filepath.IsAbs(basePath) {
+		return "", fmt.Errorf("basePath must be an absolute path")
+	}
+	if !filepath.IsAbs(targetPath) {
+		return targetPath, nil
+	}
+
+	// Construct the relative path
+	relativePath, err := filepath.Rel(basePath, targetPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to construct relative path: %w", err)
+	}
+
+	return relativePath, nil
 }
